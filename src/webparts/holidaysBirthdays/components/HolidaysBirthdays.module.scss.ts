a33faea2b086ec711f29a30d40b09a180f499bb6
Chris McNulty <cmcnulty@synozur.com>
2025-12-29 10:43:45 -0800

    Enhance error handling in RecurrenceCalculator service and update related tests for better reliability.

---
+++ src/webparts/holidaysBirthdays/components/HolidaysBirthdays.module.scss.ts
@@ -0,0 +1,18 @@
+/* tslint:disable */
+require("./HolidaysBirthdays.module.css");
+const styles = {
+  holidaysBirthdays: 'holidaysBirthdays_ec58eb7a',
+  title: 'title_ec58eb7a',
+  emptyState: 'emptyState_ec58eb7a',
+  monthHeader: 'monthHeader_ec58eb7a',
+  eventItem: 'eventItem_ec58eb7a',
+  eventImage: 'eventImage_ec58eb7a',
+  eventDate: 'eventDate_ec58eb7a',
+  eventTitle: 'eventTitle_ec58eb7a',
+  eventNotes: 'eventNotes_ec58eb7a',
+  typeBadge: 'typeBadge_ec58eb7a',
+  errorHint: 'errorHint_ec58eb7a'
+};
+
+export default styles;
+/* tslint:enable */
