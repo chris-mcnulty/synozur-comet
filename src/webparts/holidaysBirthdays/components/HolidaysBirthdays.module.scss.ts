10dcb4c25fb88dc8c26e94c2ac5037e2cbaae852
Chris McNulty <cmcnulty@synozur.com>
2025-12-29 16:45:07 -0800

    v1.1.2 fixes for memorial day recurrence

--- src/webparts/holidaysBirthdays/components/HolidaysBirthdays.module.scss.ts
+++ src/webparts/holidaysBirthdays/components/HolidaysBirthdays.module.scss.ts
@@ -1,20 +1,20 @@
 /* tslint:disable */
 require("./HolidaysBirthdays.module.css");
 const styles = {
-  holidaysBirthdays: 'holidaysBirthdays_d7778117',
-  title: 'title_d7778117',
-  emptyState: 'emptyState_d7778117',
-  monthHeader: 'monthHeader_d7778117',
-  eventItem: 'eventItem_d7778117',
-  eventImage: 'eventImage_d7778117',
-  eventDate: 'eventDate_d7778117',
-  eventTitle: 'eventTitle_d7778117',
-  eventNotes: 'eventNotes_d7778117',
-  typeBadge: 'typeBadge_d7778117',
-  errorHint: 'errorHint_d7778117',
-  footer: 'footer_d7778117',
-  footerLogo: 'footerLogo_d7778117',
-  footerText: 'footerText_d7778117'
+  holidaysBirthdays: 'holidaysBirthdays_759ae93b',
+  title: 'title_759ae93b',
+  emptyState: 'emptyState_759ae93b',
+  monthHeader: 'monthHeader_759ae93b',
+  eventItem: 'eventItem_759ae93b',
+  eventImage: 'eventImage_759ae93b',
+  eventDate: 'eventDate_759ae93b',
+  eventTitle: 'eventTitle_759ae93b',
+  eventNotes: 'eventNotes_759ae93b',
+  typeBadge: 'typeBadge_759ae93b',
+  errorHint: 'errorHint_759ae93b',
+  footer: 'footer_759ae93b',
+  footerLogo: 'footerLogo_759ae93b',
+  footerText: 'footerText_759ae93b'
 };
 
 export default styles;
