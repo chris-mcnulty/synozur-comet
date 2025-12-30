7000d3d17e995cf39278ff5b4df72e91585b573a
Chris McNulty <cmcnulty@synozur.com>
2025-12-29 18:29:46 -0800

    v1.1.5a font fix w screenshot for app catalog

---
+++ src/webparts/holidaysBirthdays/components/HolidaysBirthdays.module.scss.ts
@@ -0,0 +1,22 @@
+/* tslint:disable */
+require("./HolidaysBirthdays.module.css");
+const styles = {
+  holidaysBirthdays: 'holidaysBirthdays_665fce82',
+  title: 'title_665fce82',
+  emptyState: 'emptyState_665fce82',
+  monthHeader: 'monthHeader_665fce82',
+  eventItem: 'eventItem_665fce82',
+  eventImage: 'eventImage_665fce82',
+  eventDate: 'eventDate_665fce82',
+  eventTitle: 'eventTitle_665fce82',
+  eventNotes: 'eventNotes_665fce82',
+  typeBadge: 'typeBadge_665fce82',
+  errorHint: 'errorHint_665fce82',
+  footer: 'footer_665fce82',
+  footerLogo: 'footerLogo_665fce82',
+  footerText: 'footerText_665fce82',
+  toggleButton: 'toggleButton_665fce82'
+};
+
+export default styles;
+/* tslint:enable */
