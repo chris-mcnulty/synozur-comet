65e3edcf6aa661cece4dc7a995b89e83442fc706
Chris McNulty <cmcnulty@synozur.com>
2025-12-29 18:11:12 -0800

    v1.1.4 fixes

---
+++ src/webparts/holidaysBirthdays/components/HolidaysBirthdays.module.scss.ts
@@ -0,0 +1,21 @@
+/* tslint:disable */
+require("./HolidaysBirthdays.module.css");
+const styles = {
+  holidaysBirthdays: 'holidaysBirthdays_7d82b4e8',
+  title: 'title_7d82b4e8',
+  emptyState: 'emptyState_7d82b4e8',
+  monthHeader: 'monthHeader_7d82b4e8',
+  eventItem: 'eventItem_7d82b4e8',
+  eventImage: 'eventImage_7d82b4e8',
+  eventDate: 'eventDate_7d82b4e8',
+  eventTitle: 'eventTitle_7d82b4e8',
+  eventNotes: 'eventNotes_7d82b4e8',
+  typeBadge: 'typeBadge_7d82b4e8',
+  errorHint: 'errorHint_7d82b4e8',
+  footer: 'footer_7d82b4e8',
+  footerLogo: 'footerLogo_7d82b4e8',
+  footerText: 'footerText_7d82b4e8'
+};
+
+export default styles;
+/* tslint:enable */
